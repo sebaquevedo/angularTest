3789bcd5587b67daabc493acfbd858e96b7968c9
sebastian quevedo <swebastian.quevedo@ai-ware.cl>
2018-10-25 15:36:33 -0300

    Unit Test example

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 describe('AppComponent', () => {
+  let fixture: any;
+  let app: any;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
@@ -8,6 +10,13 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   }));
+  beforeEach(()=>{
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.debugElement.componentInstance;
+    //set your inputs of component to avoid error while testing
+    app.data = 'something';
+    fixture.detectChanges();
+  });
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
@@ -24,4 +33,11 @@ describe('AppComponent', () => {
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to testingApp!');
   }));
+  it('should say hello',()=>{
+    app.sayHello();
+    expect(app.title).toBe('Hello');
+  })
+  it('should set input of comp',()=>{
+    expect(app.data).toBe('something'); 
+  })
 });
